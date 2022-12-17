bd7b8e03b8100e16d37207e035f2b45ccf1b6400
Mark Will <me@themark.dev>
2022-12-17 19:05:54 -0300

    fix 🐞: Change message for develop

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('Hello World!');
+    return res.send('Hello World - This is Dev!');
 })
 
 app.get('/secrets', (req, res) => {
